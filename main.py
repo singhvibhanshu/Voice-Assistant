837cd5ddbd2a04e0d02c07aeafd144ad81cc3136
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-01-13 10:47:26 +0530

    sound added

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+speak("Hey there! I am your personal Voice Assistant.")
